da532e9f25d52f955eb144cf313bd4ba8c3c05de
Brian Marshall <bm.marshall@btinternet.com>
2022-04-12 11:19:46 +0100

    Most of CategoryView working well - still need to implement edit functions About to switch to Dark Theme (MainView already switched)

--- src/main/java/com/brian/webbookmarker/controllers/CategoryViewController.java
+++ src/main/java/com/brian/webbookmarker/controllers/CategoryViewController.java
@@ -1,25 +1,109 @@
 package com.brian.webbookmarker.controllers;
 
+import com.brian.webbookmarker.models.ExcelData;
+import com.brian.webbookmarker.models.ExcelDataItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Dialog;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class CategoryViewController {
+
+    @FXML
+    private ListView<String> categoryListView;
+    @FXML
+    private ListView<ExcelDataItem> titlesListView;
     @FXML
     private VBox catViewVBox;
+    @FXML
+    private TextField idTextField;
+    @FXML
+    private TextField titleTextField;
+    @FXML
+    private TextField urlTextField;
+    @FXML
+    private TextField categoryTextField;
+    @FXML
+    private TextArea descriptionTextArea;
+
+
+
     private Parent myParent;
     private Stage stage;
     private Scene scene;
+
+    public void initialize() {
+        ObservableList<String> catList;
+        List<ExcelDataItem> fullTitlesList =  new ArrayList<>();
+        ObservableList<ExcelDataItem> selectedList = FXCollections.observableArrayList();
+        catList = FXCollections.observableArrayList();
+
+        categoryListView.getSelectionModel().selectedItemProperty().addListener((ChangeListener<? super String>) (observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                String catSelected = categoryListView.getSelectionModel().getSelectedItem();
+                List<ExcelDataItem> allData = ExcelData.getInstance().getAllData();
+                selectedList.clear();
+                for (ExcelDataItem item : allData) {
+                       if (item.getCategory().equals(catSelected)){
+                           selectedList.add(item);
+                       }
+                }
+                titlesListView.getSelectionModel().selectFirst();
+            }
+        });
+
+
+        titlesListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if (newValue != null) {
+                    ExcelDataItem bookmarkItem = titlesListView.getSelectionModel().getSelectedItem();
+                    idTextField.setText(bookmarkItem.getIdAsString());
+                    titleTextField.setText(bookmarkItem.getTitle());
+                    urlTextField.setText(bookmarkItem.getUrlAddress());
+                    descriptionTextArea.setText(bookmarkItem.getDescription());
+                    categoryTextField.setText(bookmarkItem.getCategory());
+
+                }
+            }
+        });
+
+        ObservableSet<String> categories = ExcelData.getInstance().getCategories();
+        catList.addAll(categories);
+        categoryListView.setItems(catList);
+
+        categoryListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        categoryListView.getSelectionModel().selectFirst();
+        String selectedCategory = categoryListView.getSelectionModel().getSelectedItem();
+
+
+        fullTitlesList = ExcelData.getInstance().getAllData();
+        selectedList.clear();
+        for (ExcelDataItem title : fullTitlesList) {
+            if (title.getCategory().equals(selectedCategory)) {
+                selectedList.add(title);
+            }
+        }
+
+        titlesListView.setItems(selectedList);
+        titlesListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        titlesListView.getSelectionModel().selectFirst();
+    }
 
     @FXML
     public void switchToMainView(ActionEvent event) throws IOException {
